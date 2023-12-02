e63b101712bfbf344def60191855755f722ad30c
sedusik <sedusova.tanya@yandex.ru>
2023-12-02 17:22:32 +0300

    add handlers in index.php

--- public/index.php
+++ public/index.php
@@ -9,9 +9,15 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+    return $response->write('Welcome to Slim!');
 });
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
 $app->run();
